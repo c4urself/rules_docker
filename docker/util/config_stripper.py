946cb0f49238b4ae2bffdb0680d2d01efe32f972
Dmytro Popovych <drudim.ua@gmail.com>
2019-10-22 16:25:57 +0300

    FIX py3 compatibility

--- docker/util/config_stripper.py
+++ docker/util/config_stripper.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 import argparse
-import cStringIO
+import io
 import hashlib
 import json
 import os
@@ -39,6 +39,8 @@ def main():
                         required=True)
     args = parser.parse_args()
 
+    os.environ["PYTHONIOENCODING"] = "utf-8"
+
     return strip_tar(args.in_tar_path, args.out_tar_path)
 
 
@@ -58,10 +60,10 @@ def strip_tar(input, output):
         new_layers = []
         new_diff_ids = []
         for layer in image['Layers']:
-          (new_layer_name, new_diff_id) = strip_layer(os.path.join(tempdir, layer))
+            (new_layer_name, new_diff_id) = strip_layer(os.path.join(tempdir, layer))
 
-          new_layers.append(new_layer_name)
-          new_diff_ids.append(new_diff_id)
+            new_layers.append(new_layer_name)
+            new_diff_ids.append(new_diff_id)
 
         # Change the manifest to reflect the new layer name
         image['Layers'] = new_layers
@@ -101,24 +103,24 @@ def strip_layer(path):
     # working directory is one level up from where layer.tar is.
     original_dir = os.path.normpath(os.path.join(os.path.dirname(path), '..'))
 
-    buf = cStringIO.StringIO()
+    buf = io.BytesIO()
 
     # Go through each file/dir in the layer
     # Set its mtime to 0
     # If it's a file, add its content to the running buffer
     # Add it to the new gzip'd tar.
     with tarfile.open(name=path, mode='r') as it:
-      with tarfile.open(fileobj=buf, mode='w') as ot:
-        for tarinfo in it:
-          # Use a deterministic mtime that doesn't confuse other programs,
-          # e.g. Python.
-          # Also see https://github.com/bazelbuild/bazel/issues/1299
-          tarinfo.mtime = 946684800 # 2000-01-01 00:00:00.000 UTC
-          if tarinfo.isfile():
-            f = it.extractfile(tarinfo)
-            ot.addfile(tarinfo, f)
-          else:
-            ot.addfile(tarinfo)
+        with tarfile.open(fileobj=buf, encoding='utf-8', mode='w') as ot:
+            for tarinfo in it:
+                # Use a deterministic mtime that doesn't confuse other programs,
+                # e.g. Python.
+                # Also see https://github.com/bazelbuild/bazel/issues/1299
+                tarinfo.mtime = 946684800 # 2000-01-01 00:00:00.000 UTC
+                if tarinfo.isfile():
+                    f = it.extractfile(tarinfo)
+                    ot.addfile(tarinfo, f)
+                else:
+                    ot.addfile(tarinfo)
 
     # Create the new diff_id for the config
     tar = buf.getvalue()
@@ -137,8 +139,8 @@ def strip_layer(path):
     # Calculate sha of layer
     sha = hashlib.sha256(gz).hexdigest()
     new_name = 'sha256:%s' % sha
-    with open(os.path.join(original_dir, new_name), 'w') as out:
-      out.write(gz)
+    with open(os.path.join(original_dir, new_name), 'wb') as out:
+        out.write(gz)
 
     shutil.rmtree(os.path.dirname(path))
     return (new_name, diffid)
@@ -152,15 +154,15 @@ def strip_config(path, new_diff_ids):
 
     # Base container info is not required and changes every build, so delete it.
     if 'container' in config:
-      del config['container']
+        del config['container']
     if ('config' in config and
-        'Hostname' in config['config']):
-      del config['config']['Hostname']
+            'Hostname' in config['config']):
+        del config['config']['Hostname']
     if ('container_config' in config and
-        'Hostname' in config['container_config']):
-      del config['container_config']['Hostname']
+            'Hostname' in config['container_config']):
+        del config['container_config']['Hostname']
     if 'docker_version' in config:
-      del config['docker_version']
+        del config['docker_version']
     for entry in config['history']:
         entry['created'] = _TIMESTAMP
 
@@ -169,7 +171,7 @@ def strip_config(path, new_diff_ids):
         f.write(config_str)
 
     # Calculate the new file path
-    sha = hashlib.sha256(config_str).hexdigest()
+    sha = hashlib.sha256(config_str.encode("utf-8")).hexdigest()
     new_path = 'sha256:%s' % sha
     os.rename(path, os.path.join(os.path.dirname(path), new_path))
     return new_path
